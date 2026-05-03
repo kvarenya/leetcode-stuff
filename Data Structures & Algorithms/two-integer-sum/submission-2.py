02744aa1a70989c7d4b4402ce8b3c644dee50948
kvarenya <57556896+kvarenya@users.noreply.github.com>
2026-05-02 20:58:29 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        #Brute force O(n^2) solution:
+        ans = []
+
+        if (len(nums) == 2):
+            return [0, 1]
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    ans.append(i)
+                    ans.append(j)
+
+                    return ans
+            
+        
